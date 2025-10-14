b219f7cf5990a3f2cb6b0a0760e2ec302403361d
AaryaPatel1511 <aarya.18265@sakec.ac.in>
2025-10-14 22:29:17 +0530

    updated server.js

--- server.js
+++ server.js
@@ -5,29 +5,26 @@ import cors from 'cors';
 import authRoutes from './routes/auth.js';
 import transactionRoutes from './routes/transactions.js';
 
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// ✅ CORS configuration to allow frontend requests
-app.use(cors({
-  origin: "", // frontend URL
-  credentials: true
-}));
 
+app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
-// Routes
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("✅ MongoDB Connected"))
+.catch(err => console.error("❌ MongoDB Connection Error:", err));
+
+
 app.use('/auth', authRoutes);
 app.use('/transactions', transactionRoutes);
 
-// Test route
+
 app.get('/', (req, res) => res.send('Expense Tracker API Running'));
 
-// Start server
+
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
