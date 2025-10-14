f16d09e89738826caba107cbac36ab173e61fa0a
AaryaPatel1511 <aarya.18265@sakec.ac.in>
2025-10-14 21:59:00 +0530

    updated server.js

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ CORS configuration to allow frontend requests
 app.use(cors({
-    origin: process.env.FRONTEND_URL,  // dynamic frontend URL
+    origin: "http://localhost:5173",
     credentials: true
   }));
+  
   
 
 app.use(express.json());
