583179e61b6e5ea2dfd8a6d103f79c1937acef72
AaryaPatel1511 <aarya.18265@sakec.ac.in>
2025-10-14 23:51:44 +0530

    server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ CORS configuration for your deployed frontend
 app.use(cors({
-    origin: "https://expense-tracker-8s3669o5x-aarya-patel-s-projects.vercel.app",
+    origin: ["http://localhost:5173" ,"https://expense-tracker-8s3669o5x-aarya-patel-s-projects.vercel.app"],
     credentials: true
 }));
 
