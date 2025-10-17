affde0e986373284f03fb78d64d1f5a138c5057b
AaryaPatel1511 <aarya.18265@sakec.ac.in>
2025-10-17 11:08:51 +0530

    updated server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ["http://localhost:3000", "https://expense-tracker-omega-seven-13.vercel.app"],
+  origin: ["http://localhost:3000", "https://expense-tracker-pi-rouge.vercel.app"],
   credentials: true
 }));
 
